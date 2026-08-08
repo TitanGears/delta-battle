a830bd9f6083db854f289336d5e6a494229c4fe5
TitanGears <titangears.games@free.fr>
2026-08-08 19:37:33 +0200

    Add files via upload

--- soul/behaviors/movement/blue_soul_move.gd
+++ soul/behaviors/movement/blue_soul_move.gd
@@ -1,13 +1,21 @@
 extends SoulMove
 
 @export var gravity := Vector2(0, 1)
-@export var jump_speed_multiplier : float = 1.2
-@export var max_air_time : float = 0.3
+@export var jump_speed_multiplier : float = 1.5 		# 1.2
+@export var max_air_time : float = 0.45 				# 0.3
 
 var current_air_time : float = 0
 
+# Added in in order to customize more easily the soul fall direction
+## Direction towards which the Soul will fall.
+#@export var face_direction : Direction = Direction.SOUTH
+@export var jump_input_used := "up"
+
 func start() -> void:
-	set_gravity_direction(Direction.EAST)
+	set_gravity_direction(face_direction)
+	if face_direction == Direction.NORTH : jump_input_used = "down"
+	elif face_direction == Direction.EAST : jump_input_used = "left"
+	elif face_direction == Direction.WEST : jump_input_used = "right"
 	soul.motion_mode = CharacterBody2D.MOTION_MODE_GROUNDED
 
 func tick(delta:float) -> void:
@@ -26,11 +34,12 @@ func apply_speed(input : Vector2) -> Vector2:
 		sup += gravity * speed
 	return sup
 
-func get_move_rotation() -> AxisMoveType:
-	return AxisMoveType.ROTATED
+# Caused the glitch.
+#func get_move_rotation() -> AxisMoveType:
+	#return AxisMoveType.ROTATED
 
 func should_ascend() -> bool:
-	return Input.is_action_pressed("up") && current_air_time > 0.0
+	return Input.is_action_pressed(jump_input_used) && current_air_time > 0.0
 
 func set_gravity_direction(dir : Direction, multiplier : float = 1.0) -> void:
 	var dir_to_axis := {
