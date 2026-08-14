b8d4d81a2cf431bf1cf02e6f6459c08ad106cf0b
TitanGears <titangears.games@free.fr>
2026-08-14 15:45:54 +0200

    Add files via upload

--- globals/sounds/sounds.gd
+++ globals/sounds/sounds.gd
@@ -11,6 +11,8 @@ func _ready() -> void:
 	var sound_files := get_files_recursive("res://globals/sounds/sound_effects")
 	if !sound_files.is_empty():
 		for file: String in sound_files:
+			if file.ends_with(".gitignore") :
+				continue
 			var audio_player := AudioStreamPlayer.new()
 			audio_player.name = file.get_file().trim_suffix("." + file.get_extension())
 			audio_player.stream = load(file)
@@ -20,6 +22,8 @@ func _ready() -> void:
 	var music_files := get_files_recursive("res://globals/sounds/music")
 	if !music_files.is_empty():
 		for file: String in music_files:
+			if file.ends_with(".gitignore") :
+				continue
 			var audio_player := AudioStreamPlayer.new()
 			audio_player.name = file.get_file().trim_suffix("." + file.get_extension())
 			audio_player.stream = load(file)
@@ -45,10 +49,42 @@ func play(p_sound: String, p_volume := 1.0) -> void:
 	audio_player.volume_linear = p_volume
 	audio_player.play()
 
+func play_looped(p_sound : String, p_volume := 1.0) -> void :
+	if !sounds.has(p_sound) :
+		#printerr("Error in sounds.gd: Attempt to play sound, \"" + p_sound + "\", but no such sound exists.")
+		return
+	var audio_player := sounds[p_sound]
+	var stream := audio_player.stream
+	if stream is AudioStreamWAV :
+		if stream.loop_mode == AudioStreamWAV.LOOP_DISABLED :
+			stream.loop_mode = AudioStreamWAV.LOOP_FORWARD
+			# N'oublie pas d'importer en précisant si ça loop.
+			#stream.loop_end = stream.data.size() / (2 if stream.format == AudioStreamWAV.FORMAT_16_BITS else 1)
+	elif stream is AudioStreamOggVorbis :
+		stream.loop = true
+	audio_player.volume_linear = p_volume
+	audio_player.play()
+
+func stop(p_sound: String) -> void:
+	if !sounds.has(p_sound):
+		return
+	sounds[p_sound].stop()
+
+func set_pitch(p_sound: String, p_pitch: float) -> void:
+	if !sounds.has(p_sound):
+		return
+	sounds[p_sound].pitch_scale = p_pitch
+
 func set_music(p_music: String, p_volume := 1.0, p_loop := true):
 	if p_music == current_song:
 		return
 	
+	if p_music == "":    # Quick change to stop the music.
+		var previous_song := music[current_song]
+		previous_song.stop()
+		current_song = ""    # Clear current_song so playback can restart later.
+		return
+
 	if !music.has(p_music):
 		printerr("Error in sounds.gd: Attempt to play song, \"" + p_music + "\", but no such song exists.")
 		return
